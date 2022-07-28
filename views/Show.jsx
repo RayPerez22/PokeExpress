bf2229425bdc8c6b6d53fd4fdebef16aee44300e
RayPerez22 <raynyc2285@gmail.com>
2022-07-28 15:03:19 -0400

    Added dynamic anchor tags to index.jsx

---
+++ views/Show.jsx
@@ -0,0 +1,13 @@
+// const React = require('react')
+// class Show extends React.Component {
+//   render () {
+//    const pokemon = this.props.pokemon
+//     return (
+//       <div>
+//       <h1> Show Page </h1>
+        
+//       </div>
+//       )
+//      }
+//    }
+//   module.exports  = Show
