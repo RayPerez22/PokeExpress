b6bbb40b1cbe9b196e45901399c565dc25910763
RayPerez22 <raynyc2285@gmail.com>
2022-07-28 14:06:34 -0400

    Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express')
+require('dotenv').config()
+const pokemon = require('./models/pokemon.js')
 const app = express()
 const port = process.env.PORT || 3003
 
+
+//our routes
 app.get('/',(req, res) =>{
     res.send('Welcome to the Pokemon App!')
+})
+
+app.get('/pokemon/',(req, res) =>{
+    res.send('Pokemon')
 })
 
 
